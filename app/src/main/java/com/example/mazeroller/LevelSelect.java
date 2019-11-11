cd76f01ccc5b46350aa777e9163557f339f69057
Gravitas <shravmasterice842@gmail.com>
2019-11-11 16:24:42 -0500

    Main menu and level select design

---
+++ app/src/main/java/com/example/mazeroller/LevelSelect.java
@@ -0,0 +1,14 @@
+package com.example.mazeroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_select);
+    }
+}
---
+++ app/src/main/java/com/example/mazeroller/MainMenu.java
@@ -0,0 +1,29 @@
+package com.example.mazeroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button play = findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, LevelSelect.class));
+            }
+        });
+    }
+
+
+
+}
