a48403402b9c91a52835d07089c7638428257927
Jack Yu <jyu108@jhu.edu>
2019-11-11 19:52:44 -0500

    Set up home buttons for Level Select.

--- app/src/main/java/com/example/mazeroller/LevelSelect.java
+++ app/src/main/java/com/example/mazeroller/LevelSelect.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LevelSelect extends AppCompatActivity {
 
@@ -20,6 +21,15 @@ public class LevelSelect extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LevelSelect.this, Level.class));
+            }
+        });
+
+        ImageButton home = findViewById(R.id.levelSelectHome);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LevelSelect.this, MainMenu.class));
             }
         });
 
