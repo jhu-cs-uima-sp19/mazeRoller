e42a74ccb562caacff5fc79410dcea2453bd0090
raghavsambasivan <rsambasivan@gmail.com>
2019-12-06 18:36:24 -0500

    Level 3 designed preliminary

--- app/src/main/java/com/example/mazeroller/Level3.java
+++ app/src/main/java/com/example/mazeroller/Level3.java
@@ -13,23 +13,31 @@ public class Level3 extends Level {
 
     public Level3() {
 
-        horz = 10;
-        vert = 5;
-        background = R.color.yellow;
+        horz = 19;
+        vert = 10;
+        background = R.color.red;
         startColor = R.color.white;
         ballColor = R.color.blue;
-        wallColor = R.color.black;
+        wallColor = R.color.darkGrey;
         starColor = R.color.green;
         starBorderColor = R.color.black;
         pitfallColor = R.color.black;
-        endColor = R.color.red;
+        endColor = R.color.lightGrey;
 
-        Element[][] elements = {{new Start(), new Wall(),          null,       null, null,       null,       null,       null, new Star(), new Pitfall()},
-                                {       null, new Wall(),    new Star(), new Wall(), null, new Wall(),       null, new Wall(), new Wall(),    new Wall()},
-                                {       null, new Wall(), new Pitfall(),       null, null, new Wall(),       null,       null,       null,    new Wall()},
-                                {       null, new Wall(),    new Wall(), new Wall(), null, new Wall(), new Wall(), new Wall(),       null,     new End()},
-                                { new Star(),       null,          null,       null, null, new Wall(), new Wall(), new Wall(), new Wall(),    new Wall()}};
-        this.grid = elements;
+
+        Element[][] elements = {
+                {new Star(), new Pitfall(), null, null, null, new Pitfall(), null, null, null, null, null, null, null, new Pitfall(), null, null, null, null, new Pitfall()},
+                {null, null, null, new Pitfall(), null, null, null, new Wall(), new Wall(), new Wall(), new Wall(), new Pitfall(), null, null, null, new Wall(), new Wall(), null, null},
+                {new Pitfall(), new Pitfall(), new Wall(), new Wall(), new Wall(), new Wall(), new Wall(), new Pitfall(), null, new Pitfall(), new Wall(), new Wall(), new Wall(), new Wall(), new Wall(), new Pitfall(), new Wall(), new Wall(), null},
+                {new Wall(), new Wall(), new Pitfall(), new Pitfall(), new Pitfall(), new Wall(), new Pitfall(), null, null, null, new Wall(), new Wall(), new Pitfall(), new Pitfall(), new Pitfall(), null, new Pitfall(), new Pitfall(), null},
+                {new Start(), null, null, null, null, null, null, null, new Pitfall(), null, null, null, null, null, new Pitfall(), null, null, null, null},
+                {new Wall(), new Wall(), new Pitfall(), new Pitfall(), new Pitfall(), new Wall(), new Pitfall(), null, null, new Wall(), new Wall(), new Wall(), new Pitfall(), null, new Pitfall(), null, new Pitfall(), new Pitfall(), null},
+                {new End(), new Pitfall(), new Wall(), new Wall(), new Wall(), new Pitfall(), new Wall(), new Pitfall(), new Star(), new Pitfall(), new Wall(), new Star(), new Wall(), null, null, null, new Wall(), new Wall(), null},
+                {null, null, new Wall(), new Pitfall(), null, null, null, new Wall(), new Wall(), new Wall(), null, null, new Wall(), new Wall(), new Wall(), new Wall(), new Wall(), null, null},
+                {new Pitfall(), null, new Pitfall(), new Wall(), null, new Pitfall(), null, new Pitfall(), null, null, null, new Pitfall(), null, null, null, null, null, null, new Pitfall()},
+                {null, null, null, null, null, new Wall(), null, null, null, new Pitfall(), null, null, null, new Pitfall(), new Wall(), new Pitfall(), null, null, new Pitfall()}
+        };
+      this.grid = elements;
 
     }
 
